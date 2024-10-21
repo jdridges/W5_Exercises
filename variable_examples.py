10c11d1a1563f482a7a2751ea3f1c5496a7211ce
jdridges <jridges@appdev.yearup.org>
2024-10-21 15:41:25 -0500

    added variable examples exercise

---
+++ variable_examples.py
@@ -0,0 +1,33 @@
+customer_id = '750961'    #string
+customer_fname = 'John' #string
+customer_mname = 'Fredrick' #string
+customer_lname = 'Smith'    #string
+gender = 'Male' #string
+date_of_birth = '07/12/1900'    #string
+drivers_license_number = '445607918273'   #string
+auto_policy_number = '7732029000' #string
+
+
+my_fname = 'Jacob'
+my_mname = 'Daniel'
+my_lname = 'Ridges'
+birth_city = 'Lisle'
+birth_state = 'Illinois'
+
+
+
+# False	def	    if	raise^^
+# None	del	    import	return
+# True	elif^^	in	    try
+# and	else	is	    while
+# as	except	lambda^^	with
+# assert^^	finally	    nonlocal	yield^^
+# break^^	    for	    not
+# class	    form	or
+# continue	global	pass
+
+#1) raise - used to raise an error. These errors are visible in the traceback and they cancel the execution of the program if not handled properly.
+#2) elif- shorthand for else if, checks if some other condition holds when the condition in the statement is false. 
+#3) while- this statement is used to start a while loop. It continues iteration until a condition is no longer True.
+#4) lambda- A lambda function is an anonymous function. It can take any number of arguments but only have a single expression. 
+#5) nonlocal - This keyword is used in functions inside functions to create anonymous functions. 
