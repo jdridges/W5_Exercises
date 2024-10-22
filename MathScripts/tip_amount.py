ade131fbe1bc1153edb90307503e096b3a58c90d
jdridges <jridges@appdev.yearup.org>
2024-10-22 12:03:26 -0500

    tip_amount.py added to MathScripts

---
+++ MathScripts/tip_amount.py
@@ -0,0 +1,7 @@
+#the tip amount is 20% of the total bill 
+
+restaurant_bill = 195.00
+tip_percentage = .20
+tip = restaurant_bill * tip_percentage
+
+print ("The tip on a $" + str(restaurant_bill) + " restaurant bill is $" + str(tip))
