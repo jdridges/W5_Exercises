5590025ffab6962ee2a40211270718623bfe3544
jdridges <jridges@appdev.yearup.org>
2024-10-22 14:37:32 -0500

    area_of_circle added

---
+++ MathScripts/area_of_circle.py
@@ -0,0 +1,8 @@
+# the area of a circle formula is pi(r)^2
+import math
+
+pi = math.pi
+r= 3
+area_of_circle = pi*(r*r)
+
+print("The area of a circle with radius 3 is " + format(area_of_circle, ".2f"))
