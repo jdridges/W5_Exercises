fb5d2b69038d978848b74f708516d0323d87d91e
jdridges <jridges@appdev.yearup.org>
2024-10-22 15:32:42 -0500

    added c_to_f practice

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,7 @@
+#Conversion formula to fahrenheit is F= (C * 9/5) + 32
+
+celsius = 30
+
+fahrenheit_conversion = (celsius * (9/5))+32
+
+print(f"{celsius} degrees celsius is equal to {fahrenheit_conversion} degrees fahrenheit")
