017072309355db36542cd2e948fbcb4915fc20b1
jdridges <jridges@appdev.yearup.org>
2024-10-22 15:18:34 -0500

    added "fahrenheit to celsius" conversion

---
+++ MathScripts/f_to_c.py
@@ -0,0 +1,5 @@
+# The equation for farenheight to celcius is ((F - 32)*5)/ 9 = C
+fahrenheit = 100
+celsius_conversion = ((100-32)*5)/9
+
+print(str(fahrenheit)+ " degrees fahrenheit is equivalent to "+ format(celsius_conversion, ".1f")+" degrees celsius")
