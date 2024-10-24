3745efe8e93e95f7f01b53960296e1250ca25ca9
jdridges <jridges@appdev.yearup.org>
2024-10-24 16:01:18 -0500

    added first part of exercise 3.b

---
+++ SampleCollections/movie_list.py
@@ -0,0 +1,3 @@
+favorite_movies = ['Hereditary', 'Pulp Fiction', 'Star Wars', 'Stand By Me', 'Cars']
+number_of_movies = len (favorite_movies)
+print("The list " + str(favorite_movies) + " includes the " + str(number_of_movies) + " movies I'd like to watch" )
