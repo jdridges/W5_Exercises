a40dd6bcfcf1fa6afaf03e23f36918256d9e45aa
jdridges <jridges@appdev.yearup.org>
2024-11-01 09:28:57 -0500

    come back to greeting exercise

--- IfScripts/greeting.py
+++ IfScripts/greeting.py
@@ -1,14 +1,16 @@
 import datetime
 
 
-time_of_day = datetime.datetime.now().hour
-
-if time_of_day < 10:
+# time_of_day = datetime.datetime.now().hour
+time_of_day = 3
+if 23<= time_of_day < 4:
+       greeting = 'What are you doing up so late???'
+elif time_of_day < 10:
         greeting = 'Good morning'
 elif 10 <= time_of_day < 17:
         greeting = 'Good day'
-else:
-        greeting = 'Good evening'
+elif 17<= time_of_day<23:
+    greeting = 'Good evening'
 
 print(f'The hour of day is {time_of_day}')
 print(greeting)
